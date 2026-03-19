ce8af30d1791c0318994f8667b7d850a081d51ca
Essie506 <clarke506@gmail.com>
2026-03-19 06:46:29 +0000

    Update feed.js

--- feed.js
+++ feed.js
@@ -2,18 +2,21 @@ const posts = [
 
   {
     user: "Esther",
+    time: "2 min ago",
     text: "Finished HIIT session 🔥",
     image: "logo.png"
   },
 
   {
     user: "Alex",
+    time: "10 min ago",
     text: "5K run done",
     image: "logo.png"
   },
 
   {
     user: "Sam",
+    time: "1 hr ago",
     text: "Leg day complete",
     image: "logo.png"
   }
@@ -23,23 +26,41 @@ const posts = [
 
 const feed = document.getElementById("feed");
 
+
 posts.forEach(post => {
+
   const card = document.createElement("div");
+
   card.className = "post";
 
   card.innerHTML = `
 
     <div class="post-header">
 
-      <i class="fa-solid fa-user"></i>
+      <div class="avatar">
+        <i class="fa-solid fa-user"></i>
+      </div>
 
-      <span>${post.user}</span>
+      <div class="post-info">
+        <span class="username">${post.user}</span>
+        <span class="time">${post.time}</span>
+      </div>
 
     </div>
 
-    <p>${post.text}</p>
+    <div class="post-text">
+      ${post.text}
+    </div>
 
-    <img src="${post.image}">
+    <img class="post-image" src="${post.image}">
+
+    <div class="post-actions">
+
+      <span><i class="fa-solid fa-heart"></i> 12</span>
+      <span><i class="fa-solid fa-comment"></i> 3</span>
+      <span><i class="fa-solid fa-bookmark"></i></span>
+
+    </div>
 
   `;
 
