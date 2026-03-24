e9599cdc36e5a1b257bd4afb7f4fe7ef79dad390
Essie506 <clarke506@gmail.com>
2026-03-24 13:45:26 +0000

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,112 +1,86 @@
-
 document.addEventListener("DOMContentLoaded", () => {
+  const posts = [
+    {
+      user: "Esther",
+      time: "2 min ago",
+      text: "Finished HIIT session 🔥",
+      image: "logo.png"
+    },
+    {
+      user: "Alex",
+      time: "10 min ago",
+      text: "5K run done",
+      image: "logo.png"
+    },
+    {
+      user: "Sam",
+      time: "1 hr ago",
+      text: "Leg day complete",
+      image: "logo.png"
+    }
+  ];
 
-const posts = [
+  const feed = document.getElementById("feed");
 
-  {
-    user: "Esther",
-    time: "2 min ago",
-    text: "Finished HIIT session 🔥",
-    image: "logo.png"
-  },
-
-  {
-    user: "Alex",
-    time: "10 min ago",
-    text: "5K run done",
-    image: "logo.png"
-  },
-
-  {
-    user: "Sam",
-    time: "1 hr ago",
-    text: "Leg day complete",
-    image: "logo.png"
-  }
-
-];
-
-
-const feed = document.getElementById("feed");
-  
   if (feed) {
+    posts.forEach(post => {
+      const card = document.createElement("div");
+      card.className = "post";
 
-posts.forEach(post => {
+      card.innerHTML = `
+        <div class="post-header">
+          <div class="avatar">
+            <i class="fa-solid fa-user"></i>
+          </div>
 
-  const card = document.createElement("div");
+          <div class="post-info">
+            <span class="username">${post.user}</span>
+            <span class="time">${post.time}</span>
+          </div>
+        </div>
 
-  card.className = "post";
+        <div class="post-text">
+          ${post.text}
+        </div>
 
-  card.innerHTML = `
+        <img class="post-img" src="${post.image}">
 
-    <div class="post-header">
+        <div class="post-actions">
+          <span><i class="fa-solid fa-heart"></i> 12</span>
+          <span><i class="fa-solid fa-comment"></i> 3</span>
+          <span><i class="fa-solid fa-bookmark"></i></span>
+        </div>
+      `;
 
-      <div class="avatar">
-        <i class="fa-solid fa-user"></i>
-      </div>
-
-      <div class="post-info">
-        <span class="username">${post.user}</span>
-        <span class="time">${post.time}</span>
-      </div>
-
-    </div>
-
-    <div class="post-text">
-      ${post.text}
-    </div>
-
-    <img class="post-img" src="${post.image}">
-
-    <div class="post-actions">
-
-      <span><i class="fa-solid fa-heart"></i> 12</span>
-      <span><i class="fa-solid fa-comment"></i> 3</span>
-      <span><i class="fa-solid fa-bookmark"></i></span>
-
-    </div>
-
-  `;
-
-  feed.appendChild(card);
-
-});
-
-}
-
+      feed.appendChild(card);
+    });
+  }
 
   const mediaUpload = document.getElementById("mediaUpload");
   const postPreview = document.getElementById("postPreview");
 
-  if (!mediaUpload || !postPreview) return;
+  if (mediaUpload && postPreview) {
+    mediaUpload.addEventListener("change", function () {
+      postPreview.innerHTML = "";
 
-  mediaUpload.addEventListener("change", function () {
-    postPreview.innerHTML = "";
+      Array.from(this.files).forEach(file => {
+        const fileURL = URL.createObjectURL(file);
 
-    Array.from(this.files).forEach(file => {
-
-      const fileURL = URL.createObjectURL(file);
-
-      if (file.type.startsWith("image/")) {
-        const img = document.createElement("img");
-        img.src = fileURL;
-        postPreview.appendChild(img);
-      }
-
-      else if (file.type.startsWith("video/")) {
-        const video = document.createElement("video");
-        video.src = fileURL;
-        video.controls = true;
-        postPreview.appendChild(video);
-      }
-
+        if (file.type.startsWith("image/")) {
+          const img = document.createElement("img");
+          img.src = fileURL;
+          postPreview.appendChild(img);
+        } else if (file.type.startsWith("video/")) {
+          const video = document.createElement("video");
+          video.src = fileURL;
+          video.controls = true;
+          postPreview.appendChild(video);
+        }
+      });
     });
+  }
 
-  });
-
-document.addEventListener("DOMContentLoaded", () => {
   let lastScrollY = window.scrollY;
-
   const navbar = document.querySelector(".navbar");
   const bottomNav = document.querySelector(".bottom-nav");
 
@@ -114,18 +88,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const currentScrollY = window.scrollY;
 
     if (currentScrollY <= 10) {
-      navbar?.classList.remove("hide-top");
-      bottomNav?.classList.remove("hide-bottom");
+      if (navbar) navbar.classList.remove("hide-top");
+      if (bottomNav) bottomNav.classList.remove("hide-bottom");
       lastScrollY = currentScrollY;
       return;
     }
 
     if (currentScrollY > lastScrollY) {
-      navbar?.classList.add("hide-top");
-      bottomNav?.classList.add("hide-bottom");
+      if (navbar) navbar.classList.add("hide-top");
+      if (bottomNav) bottomNav.classList.add("hide-bottom");
     } else {
-      navbar?.classList.remove("hide-top");
-      bottomNav?.classList.remove("hide-bottom");
+      if (navbar) navbar.classList.remove("hide-top");
+      if (bottomNav) bottomNav.classList.remove("hide-bottom");
     }
 
     lastScrollY = currentScrollY;
