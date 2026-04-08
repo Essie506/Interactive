d938f137866be9a98e5dc863b51daae9f27ff944
Essie506 <clarke506@gmail.com>
2026-04-08 21:30:38 +0100

    Update sign-up.js

--- sign-up.js
+++ sign-up.js
@@ -1,16 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
+console.log("sign-up.js loaded");
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
