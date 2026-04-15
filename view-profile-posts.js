882e521004658732f15fced3e403676e36310ac2
Essie506 <clarke506@gmail.com>
2026-04-15 21:21:07 +0100

    Create view-profile-posts.js

---
+++ view-profile-posts.js
@@ -0,0 +1,16 @@
+const profilePosts = [
+  {
+    id: 1,
+    username: "Esther",
+    time: "2h",
+    text: "Strong session this morning.",
+    image: "images/post1.jpg"
+  },
+  {
+    id: 2,
+    username: "Esther",
+    time: "1d",
+    text: "New mobility work added today.",
+    image: ""
+  }
+];
