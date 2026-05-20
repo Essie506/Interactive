0665702efd526aeca0a69f7b1d009d53e91f902d
Essie506 <clarke506@gmail.com>
2026-05-20 21:05:22 +0100

    Create location.js

---
+++ location.js
@@ -0,0 +1,70 @@
+const profileGymInput =
+  document.getElementById(
+    "profileGymInput"
+  );
+
+let selectedPlace = null;
+
+if (profileGymInput) {
+
+  const autocomplete =
+    new google.maps.places.Autocomplete(
+      profileGymInput,
+      {
+        types: ["establishment"],
+        fields: [
+          "name",
+          "formatted_address",
+          "geometry",
+          "place_id"
+        ]
+      }
+    );
+
+  autocomplete.addListener(
+    "place_changed",
+    () => {
+
+      const place =
+        autocomplete.getPlace();
+
+      if (!place.geometry) return;
+
+      selectedPlace = {
+        name:
+          place.name || "",
+
+        address:
+          place.formatted_address || "",
+
+        lat:
+          place.geometry.location.lat(),
+
+        lng:
+          place.geometry.location.lng(),
+
+        placeId:
+          place.place_id || ""
+      };
+
+      console.log(
+        "Selected place:",
+        selectedPlace
+      );
+
+ 
+    // =========================
+    // FIREBASE SAVE (LATER)
+    // =========================
+
+    /*
+    await setDoc(
+      doc(db, "users", uid),
+      {
+        location: selectedPlace
+      }
+    );
+    */
+
+  }
+);
