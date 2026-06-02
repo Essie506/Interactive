ed6f41879da573fc32f64d93430d8650313b1e98
Essie506 <clarke506@gmail.com>
2026-06-02 22:25:28 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -13,6 +13,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 
 import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-storage.js";
+
+import {
   getFirestore,
   doc,
   setDoc,
@@ -34,10 +41,15 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 export {
   auth,
   db,
+    storage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
 
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
